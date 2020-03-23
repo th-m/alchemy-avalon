907553bf1bdabfdbad088e93377e66f9ce11ce77
Thom Valadez <thomas.valadez@getweave.com>
2020-03-22 21:27:48 -0600

    update rules

--- game.js
+++ game.js
@@ -1,39 +1,39 @@
 const rules = {
-      player_number: {
-        "5":{"good":3,"evil":2},
-        "6":{"good":4,"evil":2},
-        "7":{"good":4,"evil":3},
-        "8":{"good":5,"evil":3},
-        "9":{"good":6,"evil":3},
-        "10":{"good":6,"evil":4},
-      },
-      characters: {
-        "merlin":{"name":"merlin","alignment":"good","knownBy":['percival'], 'known':{} },
-        "mordred":{"name":"mordred","alignment":"evil","knownBy":null, 'known':{} },
-        "percival":{"name":"percival","alignment":"good","knownBy":null, 'known':{} },
-        "oberon":{"name":"oberon","alignment":"evil","knownBy":['mordred'], 'known':{} },
-        "morgana":{"name":"morgana","alignment":"evil","knownBy":['percival'], 'appearAs':'merlin', 'known':{} },
-        "minion":{"name":"minion","alignment":"evil","knownBy":['mordred','merlin'], 'known':{} },
-        "knight":{"name":"knight","alignment":"good","knownBy":null, 'known':{} }
-      },
-      characterList:{
-        '1': ["merlin"],
-        '2': ["merlin","minion"],
-        '3': ["merlin","knight","mordred"],
-        '4': ["merlin","knight","mordred","minion"],
-        '5': ["merlin","knight","knight","mordred","minion"],
-        '6': ["merlin","knight","knight","knight","mordred","minion"],
-        '7': ["merlin","knight","knight","knight","mordred","minion","minion"],
-        '8': ["merlin","percival","knight","knight","knight","mordred","minion","minion"],
-        '9': ["merlin","percival","knight","knight","knight","knight","mordred","morgana","minion"],
-        '10': ["merlin","percival","knight","knight","knight","knight","mordred","morgana","minion","minion"],
-      }
-    }
+  player_number: {
+    "5": { "good": 3, "evil": 2 },
+    "6": { "good": 4, "evil": 2 },
+    "7": { "good": 4, "evil": 3 },
+    "8": { "good": 5, "evil": 3 },
+    "9": { "good": 6, "evil": 3 },
+    "10": { "good": 6, "evil": 4 },
+  },
+  characters: {
+    "merlin": { "name": "merlin", "alignment": "good", "knownBy": ['percival'], 'known': {} },
+    "mordred": { "name": "mordred", "alignment": "evil", "knownBy": null, 'known': {} },
+    "percival": { "name": "percival", "alignment": "good", "knownBy": null, 'known': {} },
+    "oberon": { "name": "oberon", "alignment": "evil", "knownBy": ['mordred'], 'known': {} },
+    "morgana": { "name": "morgana", "alignment": "evil", "knownBy": ['percival'], 'appearAs': 'merlin', 'known': {} },
+    "minion": { "name": "minion", "alignment": "evil", "knownBy": ['mordred', 'merlin'], 'known': {} },
+    "knight": { "name": "knight", "alignment": "good", "knownBy": null, 'known': {} }
+  },
+  characterList: {
+    '1': ["merlin"],
+    '2': ["merlin", "minion"],
+    '3': ["merlin", "knight", "mordred"],
+    '4': ["merlin", "knight", "mordred", "minion"],
+    '5': ["merlin", "knight", "knight", "mordred", "minion"],
+    '6': ["merlin", "knight", "knight", "knight", "mordred", "minion"],
+    '7': ["merlin", "percival", "knight", "knight", "mordred", "morgana", "minion"],
+    '8': ["merlin", "percival", "knight", "knight", "knight", "mordred", "minion", "minion"],
+    '9': ["merlin", "percival", "knight", "knight", "knight", "knight", "mordred", "morgana", "minion"],
+    '10': ["merlin", "percival", "knight", "knight", "knight", "knight", "mordred", "morgana", "minion", "minion"],
+  }
+}
 
 function shuffle(array) {
-  if(!array)
+  if (!array)
     return false;
-    
+
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   while (0 !== currentIndex) {
@@ -51,7 +51,7 @@ function shuffle(array) {
 
 const game = {
   handleSecret: (secret) => {
-    if(!game[secret]){
+    if (!game[secret]) {
       game.addSecret(secret);
     }
     return true;
@@ -63,7 +63,7 @@ const game = {
     }
   },
   addPlayer: (secret, playerName) => {
-    if(game[secret].players.includes(playerName)) return false;
+    if (game[secret].players.includes(playerName)) return false;
     game[secret].players.push(playerName);
     return true;
   },
@@ -76,44 +76,44 @@ const game = {
     delete game[secret];
   },
   assignCharacters: (secret) => {
-    
+
     let characters = shuffle(rules.characterList[game[secret].players.length]);
-    
-    for (let i = 0; i <  characters.length; i++) {
-      let  char = JSON.parse( JSON.stringify(rules.characters[characters[i]] ) );
+
+    for (let i = 0; i < characters.length; i++) {
+      let char = JSON.parse(JSON.stringify(rules.characters[characters[i]]));
       game[secret].characters[game[secret].players[i]] = char;
-      
+
     }
-    
-    Object.entries(game[secret].characters).forEach(x=>{
-        if(x[1].knownBy){
-          x[1].knownBy.forEach( i =>{
-            Object.keys(game[secret].characters).forEach(j =>{
-              if(game[secret].characters[j].name === i ){
-                let characterName = (x[1].appearAs ? x[1].appearAs : x[1].name);
-                if( game[secret].characters[j].known[characterName] ){
-                  game[secret].characters[j].known[characterName].push(x[0] )
-                }else{
-                  game[secret].characters[j].known = {[characterName] : [x[0]] };
-                }
+
+    Object.entries(game[secret].characters).forEach(x => {
+      if (x[1].knownBy) {
+        x[1].knownBy.forEach(i => {
+          Object.keys(game[secret].characters).forEach(j => {
+            if (game[secret].characters[j].name === i) {
+              let characterName = (x[1].appearAs ? x[1].appearAs : x[1].name);
+              if (game[secret].characters[j].known[characterName]) {
+                game[secret].characters[j].known[characterName].push(x[0])
+              } else {
+                game[secret].characters[j].known = { [characterName]: [x[0]] };
               }
-            })
+            }
           })
-        }
+        })
+      }
     })
-    
+
     return game[secret].characters;
-  
+
   },
   makeTest: () => {
     game.handleSecret('test');
     for (var i = 0; i < 10; i++) {
-      game.addPlayer('test','a_'+i);
+      game.addPlayer('test', 'a_' + i);
     }
   }
-  
+
 }
-    
+
 module.exports = {
   game
 }
